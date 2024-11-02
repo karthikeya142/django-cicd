1bb3135ede8c67ae4e3090826c9b947adcb310ab
root <root@ip-172-30-1-216.us-east-2.compute.internal>
2024-11-02 07:42:51 +0000

    2 commit

--- envsetup.sh
+++ envsetup.sh
@@ -1,25 +1,32 @@
 #! /bin/bash
-if [-d "env"]
-then
-echo "Python virtual env exists"
+
+# Check if the virtual environment directory exists
+if [ -d "env" ]; then
+    echo "Python virtual env exists"
 else 
     python3 -m venv env
 fi
 
 echo $PWD
 
+# Activate the virtual environment
 source env/bin/activate
 
+# Install required packages
 pip3 install -r requirements.txt
 
-if [-d "logs"]
-then
-echo "Log folder exists"
+# Check if the logs directory exists
+if [ -d "logs" ]; then
+    echo "Log folder exists"
 else 
     mkdir logs
-    touch logs/error.log log/access.log
+    touch logs/error.log logs/access.log
 fi
 
+# Set permissions for logs directory
 sudo chmod -R 777 logs
 
 echo "envsetup finishes"
+
+# Make the script executable
+chmod +x envsetup.sh
